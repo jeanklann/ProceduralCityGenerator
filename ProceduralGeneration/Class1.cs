97b762aaaff89a23203f510156c91db84566b387
Jean Carlos Klann <jeanklann@gmail.com>
2017-04-26 16:08:21 -0300

    First commit

---
+++ ProceduralGeneration/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace ProceduralGeneration
+{
+    public class Class1{
+        private static Bitmap bm = new Bitmap(500, 500, System.Drawing.Imaging.PixelFormat.Format32bppPArgb);
+        public static void Main(string[] args) {
+            PerlinNoise perlin = new PerlinNoise(564165);
+            for(int x = 0; x < 500; x++) {
+                for(int y = 0; y < 500; y++) {
+                    double v = perlin.Noise(x, y, 0) * 255f;
+                    bm.SetPixel(x, y, Color.FromArgb((int)v,0,0));
+                    Console.WriteLine("X, Y, V: "+ x + ", "+ y + ": "+v);
+                }
+                Console.WriteLine();
+            }
+            bm.Save("D:\\teste.png");
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ProceduralGeneration/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ProceduralGeneration")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ProceduralGeneration")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9baa0660-9cdd-4029-afc6-14b2b822c24c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
